a96d0a1d476c4156a60b6cfff89d51a1d3cc6fee
Aayush <aayushbatham210036@acropolis.in>
2023-10-08 15:19:36 +0530

    page edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,24 @@ import logo from "./Ts.png";
 function Navbar() {
   return (
     <>
-      <header className="flex justify-center h-[10vh] items-center">
-        <nav className="bg-[#FFFFFF] p-4 rounded-2xl h-[75%] lg:w-[25%] w-[60%]">
+      <header className="flex justify-evenly h-[10vh] items-center header">
+        <nav className="p-4 rounded-2xl lg:w-[25%] w-[60%] nav mt-4">
           <ul className="flex items-center justify-evenly">
             <li>
               <img src={logo} className="w-[85%] h-[85%] "></img>
             </li>
             <li>
-              <button className="transform transition duration-500 py-2 px-3 hover:bg-[#FDF1E8] rounded-xl">
+              <button className="transform transition duration-500 py-2 px-3 hover:bg-[#ffffff] rounded-xl">
                 Features
               </button>
             </li>
             <li>
-              <button className="transform transition duration-500 py-2 px-3 hover:bg-[#FDF1E8] rounded-xl ">
+              <button className="transform transition duration-500 py-2 px-3 hover:bg-[#ffffff] rounded-xl ">
                 About
               </button>
             </li>
             <li>
-              <button className="transform transition duration-500 py-2 px-3 hover:bg-[#FDF1E8] rounded-xl">
+              <button className="transform transition duration-500 py-2 px-3 hover:bg-[#ffffff] rounded-xl">
                 Learn
               </button>
             </li>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import "./Landing.css";
 import Bg from "../components/HeroBg";
+import Lottie from "lottie-react";
+import Lottie1 from "../lotties/landing lottie 1.json";
+import Lottie2 from "../lotties/landing lottie 2.json";
 
 const Landing = () => {
   return (
@@ -21,17 +24,39 @@ const Landing = () => {
           <button className="underline">Sign in</button>
         </p>
       </section>
-      <section className="px-[10%] h-[100vh]">
+      <section className="px-[10%]">
         <h1 className="text-[3rem]">
           Welcome to <span className="font-semibold">Truly Hustle</span>
         </h1>
-        <p>
-          At Truly Hustle, we provide a vibrant and comfortable workspace
-          environment for like-minded individuals to come together, collaborate,
-          and amplify their skills. Our spaces are designed to encourage
-          creativity, productivity, and foster brilliant connections.
-        </p>
+        <div className="h-[40vh] flex justify-between">
+          <div className="w-[40%] flex items-center">
+            <p className="font-medium text-lg">
+              At Truly Hustle, we provide a vibrant and comfortable workspace
+              environment for like-minded individuals to come together,
+              collaborate, and amplify their skills. Our spaces are designed to
+              encourage creativity, productivity, and foster brilliant
+              connections.
+            </p>
+          </div>
+          <div className="w-[35%] h-1 flex flex-col gap-14">
+            <Lottie animationData={Lottie1} loop={true} />
+          </div>
+        </div>
+        <div className="h-[40vh] flex justify-between">
+          <div className="w-[35%] h-1 flex flex-col gap-14">
+            <Lottie animationData={Lottie2} loop={true} />
+          </div>
+          <div className="w-[40%] flex items-center">
+            <p className="font-medium text-lg">
+              Featuring premium facilities, cutting-edge technology, and
+              flexible solutions, Truly Hustle is designed to meet the specific
+              needs of freelancers, entrepreneurs, startups, and remote teams.
+              Unleash your potential and let your ideas flourish.
+            </p>
+          </div>
+        </div>
       </section>
+      <section></section>
     </div>
   );
 };
